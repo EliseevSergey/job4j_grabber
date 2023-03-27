37d6fa6b0763258c9c09e293887971d72dc9c37c
Sergey Eliseev <s.eliseev1@gmail.com>
2023-03-27 23:49:51 +0300

    0. Техническое задание. Агрегатор Java Вакансий. [#260357]

---
+++ src/main/java/ru/job4j/Hello.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Привет мир");
+        System.out.println("Hello world");
+    }
+}
--- src/main/java/ru/job4j/HelloWorldSec.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j;
-
-public class HelloWorldSec {
-    public static void main(String[] args) {
-        System.out.println("Hello world 2");
-    }
-}
