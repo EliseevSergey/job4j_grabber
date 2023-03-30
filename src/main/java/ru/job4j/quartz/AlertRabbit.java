d460e48fd4785db613ebfe819383005c656b8ed0
Sergey Eliseev <s.eliseev1@gmail.com>
2023-03-30 06:45:05 +0300

    1. Quartz [#175122]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -2,19 +2,34 @@ package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+    private static Properties loadCfg() {
+        Properties cfg = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
+            cfg.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cfg;
+    }
+
     public static void main(String[] args) {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
-                    .repeatForever();
+                    .withIntervalInSeconds(Integer
+                            .parseInt(loadCfg().getProperty("rabbit.interval")))
+                    .withRepeatCount(10);
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
@@ -28,7 +43,8 @@ public class AlertRabbit {
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) {
-            System.out.println("Rabbit runs here ...");
+            System.out.printf("Rabbit runs here with frequency: %S second",
+                    Integer.parseInt(loadCfg().getProperty("rabbit.interval")));
         }
     }
 }
