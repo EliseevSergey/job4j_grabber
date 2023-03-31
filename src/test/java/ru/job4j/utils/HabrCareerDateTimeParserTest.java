18611ecf024732ad31a2f7d8cec88593484b239a
Sergey Eliseev <s.eliseev1@gmail.com>
2023-04-01 00:09:23 +0300

    2.1. Преобразование даты [#289476]

--- src/test/java/ru/job4j/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/utils/HabrCareerDateTimeParserTest.java
@@ -3,10 +3,9 @@ package ru.job4j.utils;
 import org.junit.jupiter.api.*;
 import java.time.LocalDateTime;
 
-class HabrCareerDateTimeParserTest {
-
+public class HabrCareerDateTimeParserTest {
     @Test
-    void whenOkparse() {
+    void whenOkParse() {
         String in = "2023-03-31T17:25:47+03:00";
         LocalDateTime expected =
                 LocalDateTime.of(2023, 3, 31,  17,  25, 47, 0);
