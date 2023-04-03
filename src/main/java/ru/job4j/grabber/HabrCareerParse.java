836ee0ce53f274a635776baec5055a3bce11f443
Sergey Eliseev <s.eliseev1@gmail.com>
2023-04-04 02:08:33 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -9,12 +9,16 @@ import ru.job4j.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK =
             String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
-    private static final int PAGES_QTY = 5;
+    private static final int PAGES_QTY = 1;
 
     public static void main(String[] args) throws IOException {
         for (int p = 1; p <= PAGES_QTY; p++) {
@@ -32,8 +36,38 @@ public class HabrCareerParse {
                 var timeParser = new HabrCareerDateTimeParser();
                 LocalDateTime localDateTime = timeParser.parse(dataTime);
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", vacancyName, link, localDateTime);
+                String description = null;
+                try {
+                    description = retrieveDescription(link);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("%s %s %s %s%n", vacancyName, link, localDateTime, description);
             });
         }
+    }
+
+    private static String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document page = connection.get();
+        Elements rows = page.select(".style-ugc");
+        Scanner sc = new Scanner(rows.get(0).toString());
+        Pattern delimiter = Pattern.compile("</?\\w+>");
+        sc.useDelimiter(delimiter);
+        List<String> list = new ArrayList<>();
+        while (sc.hasNext()) {
+            String str = sc.next();
+            if (!str.isBlank()) {
+                list.add(str);
+            }
+        }
+        list.remove(0);
+        StringBuilder sb = new StringBuilder();
+        list.forEach(s -> {
+            sb.append(s);
+            sb.append(System.lineSeparator());
+            }
+        );
+        return sb.toString();
     }
 }
